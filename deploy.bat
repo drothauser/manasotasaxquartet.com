766deb5ef7d74bc8efcd2732377835cea237eb11
Doug Rothauser <drothauser@yahoo.com>
2024-12-21 16:30:40 -0500

    initial file transfer

---
+++ deploy.bat
@@ -0,0 +1,7 @@
+
+aws s3 cp . s3://manasotasaxquartet.com/  --recursive --exclude "*" --include "*.html" 
+aws s3 cp . s3://manasotasaxquartet.com/  --recursive --exclude "*" --include "*.js" 
+aws s3 cp . s3://manasotasaxquartet.com/  --recursive --exclude "*" --include "*.css" 
+aws s3 cp . s3://manasotasaxquartet.com/  --recursive --exclude "*" --include "videos/*.mp4" 
+aws s3 cp . s3://manasotasaxquartet.com/  --recursive --exclude "*" --include "images/*.jpg" 
+aws s3 cp . s3://manasotasaxquartet.com/  --recursive --exclude "*" --include "images/*.png" 
---
+++ header.js
@@ -0,0 +1,11 @@
+// Header section - See https://www.w3schools.com/howto/howto_js_sidenav.asp
+
+var headerHTML = ''
+headerHTML += '<a class="header" href="index.html">'
+headerHTML += '<h1>The Manasota Sax Quartet</h1>'
+headerHTML += '<div>'
+headerHTML += '<img src="images/ManasotaSaxQuartetGroupPic-24-10-01.jpg" alt="The Manasota Sax Quartet" title="The Manasota Sax Quartet">'
+headerHTML += '</div>'
+headerHTML += '</a>'
+
+document.write(headerHTML)  
