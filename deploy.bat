a317907792e2cc06214244d416c38650abb6bae3
Doug Rothauser <drothauser@yahoo.com>
2025-11-21 11:21:25 -0500

    Modify homepage and add .ico files to deploy script

--- deploy.bat
+++ deploy.bat
@@ -2,6 +2,8 @@
 aws s3 cp . s3://manasotasaxquartet.com/  --recursive --exclude "*" --include "*.html" 
 aws s3 cp . s3://manasotasaxquartet.com/  --recursive --exclude "*" --include "*.js" 
 aws s3 cp . s3://manasotasaxquartet.com/  --recursive --exclude "*" --include "*.css" 
+aws s3 cp . s3://manasotasaxquartet.com/  --recursive --exclude "*" --include "*.ico" 
 aws s3 cp . s3://manasotasaxquartet.com/  --recursive --exclude "*" --include "videos/*.mp4" 
 aws s3 cp . s3://manasotasaxquartet.com/  --recursive --exclude "*" --include "images/*.jpg" 
 aws s3 cp . s3://manasotasaxquartet.com/  --recursive --exclude "*" --include "images/*.png" 
+
