07c52d4cc0c59642832f0077a1b88c501e21f057
Doug Rothauser <drothauser@yahoo.com>
2025-12-06 14:45:31 -0500

    Use more up to date HTML and Javascript techniques

--- footer.js
+++ footer.js
@@ -1,22 +1,11 @@
-// Footer section - See https://www.w3schools.com/howto/howto_js_sidenav.asp
-
-var footerHTML = ''
-footerHTML += '<hr/>'
-footerHTML += '<div class="footer">'
-footerHTML += '<a href="index.html">Home</a> | '
-footerHTML += '<a href="personnel.html">Personnel</a> | '
-footerHTML += '<a href="https://www.youtube.com/channel/UCXAAWGf1NkOcLn8zd8leNMw" target="_blank">YouTube Channel</a> | '
-footerHTML += '<a href="schedule.html">Upcoming Performances</a> | '
-footerHTML += '<a href="contact.html">Contact</a>'
-footerHTML += '</div>'
-
-document.write(footerHTML) 
-
-var bibleLinks = ''
-bibleLinks += '<script src="https://www.biblegateway.com/public/link-to-us/tooltips/bglinks.js" type="text/javascript"></script>'
-bibleLinks += '<script type="text/javascript">'
-bibleLinks += '    BGLinks.version = "ESV";'
-bibleLinks += '    BGLinks.linkVerses();'
-bibleLinks += '</script>'
-
-document.write(bibleLinks) 
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("footer.html")
+    .then(response => {
+      if (!response.ok) throw new Error("Footer file not found");
+      return response.text();
+    })
+    .then(data => {
+      document.getElementById("footer").innerHTML = data;
+    })
+    .catch(err => console.error("Error loading footer:", err));
+});
--- header.js
+++ header.js
@@ -1,11 +1,26 @@
-// Header section - See https://www.w3schools.com/howto/howto_js_sidenav.asp
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("header.html")
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById("header").innerHTML = data;
 
-var headerHTML = ''
-headerHTML += '<a class="header" href="index.html">'
-headerHTML += '<h1>The Manasota Sax Quartet</h1>'
-headerHTML += '<div>'
-headerHTML += '<img src="images/ManasotaSaxQuartetGroupPic-24-10-01.jpg" alt="The Manasota Sax Quartet" title="The Manasota Sax Quartet">'
-headerHTML += '</div>'
-headerHTML += '</a>'
+      // Slideshow logic after header is injected
+      const images = [
+        "images/MSQ-Christimas-25-12-02-Work.jpg",
+        "images/ManasotaSaxQuartetGroupPic-24-10-01.jpg"
+      ];
+      const interval = 10000;
+      let index = 0;
+      const slideshow = document.getElementById("slideshow");
 
-document.write(headerHTML)  
+      setInterval(() => {
+        slideshow.style.opacity = 0;
+        setTimeout(() => {
+          index = (index + 1) % images.length;
+          slideshow.src = images[index];
+          slideshow.style.opacity = 1;
+        }, 1000);
+      }, interval);
+    });
+});
+
