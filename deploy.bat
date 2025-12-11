ef59534ae6392abfe1a6ed29bac493dd75916ec0
Doug Rothauser <drothauser@yahoo.com>
2025-12-11 10:54:12 -0500

    Add meta name=viewport to pages

--- deploy.bat
+++ deploy.bat
@@ -9,3 +9,6 @@ aws s3 cp . s3://manasotasaxquartet.com/  --recursive --exclude "*" --include "v
 aws s3 cp . s3://manasotasaxquartet.com/  --recursive --exclude "*" --include "images/*.jpg" 
 aws s3 cp . s3://manasotasaxquartet.com/  --recursive --exclude "*" --include "images/*.png" 
 
+aws cloudfront create-invalidation --distribution-id E14Y2782MKOXR8 --paths "/*" --no-cli-pager
+aws cloudfront create-invalidation --distribution-id E3AE300A8ORGOD --paths "/*" --no-cli-pager
+
